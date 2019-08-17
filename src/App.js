d096f89b7bde9430fd83aee385b6e2d69a6fe77b
Eleazar <erondon@gmail.com>
2019-08-16 23:31:20 -0400

    done

--- src/App.js
+++ src/App.js
@@ -2,24 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = (props) =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="container">
+  <div className="row">
+
+    <div className="col-sm digits text-center">
+    <i class="fa fa-clock-o" aria-hidden="true"></i>{props.digits}
     </div>
+  </div>
+</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,48 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let counter = 0;
+let digit = '';
+//let counter2 = 0;
+//let counter3 = 0;
+//let counter4 = 0;
+
+function countDigits (n){
+
+    if (n < 10){
+        digit = '000' + counter.toString();
+        return digit
+     }
+     else if (n<100){
+         digit = '00' + counter.toString();
+         return digit
+     }
+     else if(n < 1000){
+         digit = '0' + counter.toString();
+         return digit
+     }
+}
+setInterval(()=>{
+
+ 
+    
+
+    counter++;
+
+    ReactDOM.render(
+
+        <App 
+        
+        digits = {countDigits(counter)}
+            
+        
+        
+        
+        />, document.getElementById('root'));
+
+}, 1000)
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
